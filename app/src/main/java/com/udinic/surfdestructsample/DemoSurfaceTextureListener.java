58990564c7bd117fad50f125780509032e4136c7
Udi Cohen <udinic@fb.com>
2017-02-19 14:59:11 -0800

    RV activity is ready

---
+++ app/src/main/java/com/udinic/surfdestructsample/DemoSurfaceTextureListener.java
@@ -0,0 +1,70 @@
+package com.udinic.surfdestructsample;
+
+import android.content.Context;
+import android.graphics.SurfaceTexture;
+import android.media.MediaPlayer;
+import android.util.Log;
+import android.view.Surface;
+import android.view.TextureView;
+
+class DemoSurfaceTextureListener implements TextureView.SurfaceTextureListener {
+
+    private final Context mContext;
+
+    private MediaPlayer mMediaPlayer;
+    private final int mPosition;
+
+    DemoSurfaceTextureListener(Context context, int position) {
+        mContext = context;
+        mPosition = position;
+    }
+
+    @Override
+    public void onSurfaceTextureAvailable(
+            SurfaceTexture surfaceTexture,
+            int width,
+            int height) {
+        if (mMediaPlayer == null) {
+            Surface surface = new Surface(surfaceTexture);
+            mMediaPlayer = MediaPlayer.create(mContext, R.raw.cat);
+            mMediaPlayer.setSurface(surface);
+            mMediaPlayer.setLooping(true);
+            mMediaPlayer.start();
+            Log.d("udinic", "onSurfaceTextureAvailable position[" + mPosition
+                    + "] surfaceTexture[" + Integer.toHexString(surfaceTexture.hashCode())
+                    + "] surface[" + Integer.toHexString(surface.hashCode())
+                    + "] mplayer[" + Integer.toHexString(mMediaPlayer.hashCode()) + "]");
+        }
+    }
+
+    @Override
+    public void onSurfaceTextureSizeChanged(
+            SurfaceTexture surfaceTexture,
+            int width,
+            int height) {
+
+    }
+
+    @Override
+    public boolean onSurfaceTextureDestroyed(SurfaceTexture surfaceTexture) {
+        /**
+         * Note: This never gets called while scrolling the ListView, but gets called when one of
+         * the TextureViews in the RecyclerView goes offscreen! This is because (if memory serves
+         * me) RecyclerView calls removeView(child) while ListView calls
+         * child.dispatchStartTemporaryDetach()
+         */
+        Log.d("udinic", "onSurfaceTextureDestroyed position[" + mPosition
+                + "] surfaceTexture[" + Integer.toHexString(surfaceTexture.hashCode())
+                + "] mplayer[" + Integer.toHexString(mMediaPlayer.hashCode()) + "]");
+
+        mMediaPlayer.release();
+        surfaceTexture.release();
+        mMediaPlayer = null;
+        return false;
+    }
+
+    @Override
+    public void onSurfaceTextureUpdated(SurfaceTexture surfaceTexture) {
+
+    }
+}
--- app/src/main/java/com/udinic/surfdestructsample/RecyclerViewActivity.java
+++ app/src/main/java/com/udinic/surfdestructsample/RecyclerViewActivity.java
@@ -1,23 +1,85 @@
 package com.udinic.surfdestructsample;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.TextureView;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+
+import static android.view.ViewGroup.LayoutParams.MATCH_PARENT;
+import static android.view.ViewGroup.LayoutParams.WRAP_CONTENT;
 
 public class RecyclerViewActivity extends AppCompatActivity {
+
+    private static final int LIST_SIZE = 15;
+    private RecyclerView.Adapter<DemoViewHolder> adapter;
+    private DemoSurfaceTextureListener[] mSurfaceTextureListeners =
+            new DemoSurfaceTextureListener[LIST_SIZE];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recycler_view);
 
-
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycler);
+        initRV(this, recyclerView);
     }
 
+    private void initRV(final Context context, RecyclerView recyclerView) {
+        LinearLayoutManager layoutManager = new LinearLayoutManager(context);
+        layoutManager.setItemPrefetchEnabled(false);
+        recyclerView.setLayoutManager(layoutManager);
+//        recyclerView.setItemAnimator(null);
+        adapter = new RecyclerView.Adapter<DemoViewHolder>() {
+            @Override
+            public DemoViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+                FrameLayout itemLayout = new FrameLayout(context);
+
+                TextureView textureView = new TextureView(context);
+                itemLayout.addView(textureView, new FrameLayout.LayoutParams(MATCH_PARENT, MATCH_PARENT));
+
+                TextView textPosition = new TextView(context);
+                textPosition.setTextSize(30);
+                textPosition.setTextColor(Color.WHITE);
+                FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(WRAP_CONTENT, WRAP_CONTENT);
+                itemLayout.addView(textPosition, params);
+
+                itemLayout.setLayoutParams(new RecyclerView.LayoutParams(1000, 500));
+                return new DemoViewHolder(itemLayout, textureView, textPosition);
+            }
+
+            @Override
+            public void onBindViewHolder(DemoViewHolder holder, int position) {
+                if (mSurfaceTextureListeners[position] == null) {
+                    mSurfaceTextureListeners[position] =
+                            new DemoSurfaceTextureListener(context, position);
+                }
+                holder.textureView.setSurfaceTextureListener(mSurfaceTextureListeners[position]);
+                holder.textPosition.setText(String.valueOf(position));
+            }
+
+            @Override
+            public int getItemCount() {
+                return LIST_SIZE;
+            }
+
+            @Override
+            public long getItemId(int position) {
+                return position;
+            }
+        };
+//        adapter.setHasStableIds(true);
+        recyclerView.setAdapter(adapter);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -30,12 +92,12 @@ public class RecyclerViewActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.action_stableid:
-                Log.d("udinic", "stable ID checked=" + item.isChecked());
-                if (item.isChecked()) {
-                    item.setChecked(true);
-                } else {
-                    item.setChecked(false);
-                }
+//                Log.d("udinic", "stable ID checked=" + item.isChecked());
+//                if (item.isChecked()) {
+//                    item.setChecked(true);
+//                } else {
+//                    item.setChecked(false);
+//                }
                 return true;
             case R.id.action_switch_to_lv:
                 Log.d("udinic", "Switch" + item);
@@ -44,4 +106,18 @@ public class RecyclerViewActivity extends AppCompatActivity {
                 return super.onOptionsItemSelected(item);
         }
     }
+
+    private static class DemoViewHolder extends RecyclerView.ViewHolder {
+        FrameLayout itemLayout;
+        TextureView textureView;
+        TextView textPosition;
+
+        public DemoViewHolder(FrameLayout itemLayout, TextureView textureView, TextView textPosition) {
+            super(itemLayout);
+            this.itemLayout = itemLayout;
+            this.textureView = textureView;
+            this.textPosition = textPosition;
+        }
+    }
+
 }
