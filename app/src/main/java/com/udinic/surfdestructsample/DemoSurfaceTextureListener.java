f4739604ebbae3084b545d2cdc85c2b410fe5b70
Udi Cohen <udinic@fb.com>
2017-06-14 12:20:04 -0400

    Removing redundant code

--- app/src/main/java/com/udinic/surfdestructsample/DemoSurfaceTextureListener.java
+++ app/src/main/java/com/udinic/surfdestructsample/DemoSurfaceTextureListener.java
@@ -52,40 +52,26 @@ class DemoSurfaceTextureListener implements TextureView.SurfaceTextureListener {
     }
 
     @Override
-    public void onSurfaceTextureSizeChanged(
-            SurfaceTexture surfaceTexture,
-            int width,
-            int height) {
-
-    }
-
-    @Override
     public boolean onSurfaceTextureDestroyed(SurfaceTexture surfaceTexture) {
-        /**
-         * Note: This never gets called while scrolling the ListView, but gets called when one of
-         * the TextureViews in the RecyclerView goes offscreen! This is because (if memory serves
-         * me) RecyclerView calls removeView(child) while ListView calls
-         * child.dispatchStartTemporaryDetach()
-         */
         Log.d("udinic", "onSurfaceTextureDestroyed position[" + mPosition
                 + "] surfaceTexture[" + Integer.toHexString(surfaceTexture.hashCode())
                 + "] mplayer[" + (mMediaPlayer == null ? "NA" : Integer.toHexString(mMediaPlayer.hashCode())) + "]");
 
         cleanup();
-        surfaceTexture.release();
         if (mIndicator != null) {
             mIndicator.removeCallbacks(mRunnableInvisible);
             mIndicator.setBackgroundColor(Color.RED);
             mIndicator.postDelayed(mRunnableInvisible, 50);
         }
 
-        return false;
+        return true;
     }
 
     @Override
-    public void onSurfaceTextureUpdated(SurfaceTexture surfaceTexture) {
+    public void onSurfaceTextureSizeChanged(SurfaceTexture surfaceTexture, int width, int height) {}
 
-    }
+    @Override
+    public void onSurfaceTextureUpdated(SurfaceTexture surfaceTexture) {}
 
     public void cleanup() {
         if (mMediaPlayer != null) {
